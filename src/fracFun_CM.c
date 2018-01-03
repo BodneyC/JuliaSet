00781157ff4ccd227e216ddba66618a801f3fa5e
BodneyC <benjcarrington@gmail.com>
2018-01-03 14:51:53 +0000

    Updated and reformatted

--- src/fracFun_CM.c
+++ src/fracFun_CM.c
@@ -18,30 +18,30 @@ long iterator(Complex c, double im, double re);
 
 int main(int argc, char* argv[])
 {
-	int *send_arr, *full_arr;
-	int Y_start, X_start, CUR_CHUNK, CHUNK_SQUARED;
-	int i, j, k, LOOPCOUNT;
+    int *send_arr, *full_arr;
+    int Y_start, X_start, CUR_CHUNK, CHUNK_SQUARED;
+    int i, j, k, LOOPCOUNT;
     int NUM_CHUNKS = (FULL_WIDTH / CHUNK_WIDTH) * (FULL_WIDTH / CHUNK_WIDTH);
     int NUM_CHUNKS_REMAINING = 0;
     int disp = 0;
     FILE* img;
     Complex c;
     int pixel_YX[2];
-	/** Timing variables */
-	double start, stop;
-	float elapsed_time;
+    /** Timing variables */
+    double start, stop;
+    float elapsed_time;
 
     /** MPI specific variables */
-	MPI_Status status;
+    MPI_Status status;
     MPI_Request request;
-	int rankID, numProcs, numSlaves;
+    int rankID, numProcs, numSlaves;
 
     /** Initialisation of MPI environment */
-	MPI_Init(&argc, &argv); 
-	MPI_Barrier(MPI_COMM_WORLD); 
-	MPI_Comm_size(MPI_COMM_WORLD, &numProcs); 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rankID); 
-	MPI_Barrier(MPI_COMM_WORLD); 
+    MPI_Init(&argc, &argv);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rankID);
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Datatype CHUNKxCHUNK, CHUNKxCHUNK_RE;
 
     /** Variables for type creation */
@@ -53,21 +53,21 @@ int main(int argc, char* argv[])
 
     /** Create CHUNK by CHUNK type */
     MPI_Type_create_subarray(
-            2, // Dimensions
-            full_sizes, // Full size
-            sub_sizes, // Interior sizes
-            starting, // Begining offset
-            MPI_ORDER_C, // Ordering in mem
-            MPI_INT, // Data type
-            &CHUNKxCHUNK
-            );
+        2, // Dimensions
+        full_sizes, // Full size
+        sub_sizes, // Interior sizes
+        starting, // Begining offset
+        MPI_ORDER_C, // Ordering in mem
+        MPI_INT, // Data type
+        &CHUNKxCHUNK
+    );
     /** Specify offset */
     MPI_Type_create_resized(
-            CHUNKxCHUNK,
-            0, // Lower-boumd
-            CHUNK_WIDTH * sizeof(int), // Upper-bound (starting offset of next block)
-            &CHUNKxCHUNK_RE
-            );
+        CHUNKxCHUNK,
+        0, // Lower-boumd
+        CHUNK_WIDTH * sizeof(int), // Upper-bound (starting offset of next block)
+        &CHUNKxCHUNK_RE
+    );
     /** Commit type to be used */
     MPI_Type_commit(&CHUNKxCHUNK_RE);
 
@@ -75,37 +75,37 @@ int main(int argc, char* argv[])
     c.re = -.4;
     c.im = .6;
 
-	send_arr = (int  *)malloc(CHUNK_WIDTH * CHUNK_WIDTH * sizeof(int));
+    send_arr = (int  *)malloc(CHUNK_WIDTH * CHUNK_WIDTH * sizeof(int));
 
     /** Master process create full array and initial file IO */
-    if(rankID == 0)
-    {
+    if(rankID == 0) {
         if(rankID == 0)
             printf("Runtime Stats:\n\tNum Procs:\t%d\n\n", numProcs);
 
         full_arr = (int  *)malloc(FULL_WIDTH * FULL_WIDTH * sizeof(int));
+
         for(i = 0; i < FULL_WIDTH * FULL_WIDTH; i++)
             full_arr[i] = 5; // RANDOM VALUE
 
-		img=fopen("image_out.ppm","w");
-        if(img == NULL)
-        {
+        img = fopen("image_out.ppm", "w");
+
+        if(img == NULL) {
             printf("Could not open handle to image\n");
             return 1;
         }
 
-		fprintf(img,"P6\n%d %d 255\n", FULL_WIDTH, FULL_WIDTH);
+        fprintf(img, "P6\n%d %d 255\n", FULL_WIDTH, FULL_WIDTH);
     }
 
     /** Start MPI timer */
     start = MPI_Wtime();
 
     /** Grid-stride loop */
-    for(LOOPCOUNT = 0; LOOPCOUNT < NUM_CHUNKS; LOOPCOUNT += numProcs)
-    {
+    for(LOOPCOUNT = 0; LOOPCOUNT < NUM_CHUNKS; LOOPCOUNT += numProcs) {
         /** Work out chunk info */
         CUR_CHUNK = LOOPCOUNT + rankID;
         CHUNK_SQUARED = CHUNK_WIDTH * CHUNK_WIDTH;
+
         if(CUR_CHUNK > NUM_CHUNKS)
             CHUNK_SQUARED = 0;
 
@@ -114,34 +114,30 @@ int main(int argc, char* argv[])
 #endif
 
         /** Pixel co-ord based on chunk number */
-        pixel_YX[0] = (CUR_CHUNK / (FULL_WIDTH / CHUNK_WIDTH)) * CHUNK_WIDTH; 
-        pixel_YX[1] = (CUR_CHUNK % (FULL_WIDTH / CHUNK_WIDTH)) * CHUNK_WIDTH; 
+        pixel_YX[0] = (CUR_CHUNK / (FULL_WIDTH / CHUNK_WIDTH)) * CHUNK_WIDTH;
+        pixel_YX[1] = (CUR_CHUNK % (FULL_WIDTH / CHUNK_WIDTH)) * CHUNK_WIDTH;
 
         /** Iterate over equation for each pixel in chunk */
-        for(i = 0; i < CHUNK_WIDTH; i++)
-        {
-            for(j = 0; j < CHUNK_WIDTH; j++)
-            {
+        for(i = 0; i < CHUNK_WIDTH; i++) {
+            for(j = 0; j < CHUNK_WIDTH; j++) {
                 send_arr[(i * CHUNK_WIDTH) + j] = iterator(
-						c,
-                       -(((pixel_YX[0] + i) - (FULL_WIDTH / 2)) / (double) FULL_WIDTH) * 2,
-                       (((pixel_YX[1] + j) - (FULL_WIDTH / 2)) / (double) FULL_WIDTH) * 2
-                );
+                                                      c,
+                                                      -(((pixel_YX[0] + i) - (FULL_WIDTH / 2)) / (double) FULL_WIDTH) * 2,
+                                                      (((pixel_YX[1] + j) - (FULL_WIDTH / 2)) / (double) FULL_WIDTH) * 2
+                                                  );
             }
         }
 
         /** Calculate displacements */
-        if(rankID == 0)
-        {
-            for(i = 0; i < numProcs; i++)
-            {
+        if(rankID == 0) {
+            for(i = 0; i < numProcs; i++) {
                 if((disp + i) % (FULL_WIDTH / CHUNK_WIDTH) == 0 && disp + i != 0)
                     disp += (FULL_WIDTH / CHUNK_WIDTH) * (CHUNK_WIDTH - 1);
 
                 displs[i] = disp + i;
-                sendcounts[i] = CUR_CHUNK + i > NUM_CHUNKS ? 0: CHUNK_WIDTH * CHUNK_WIDTH;
+                sendcounts[i] = CUR_CHUNK + i > NUM_CHUNKS ? 0 : CHUNK_WIDTH * CHUNK_WIDTH;
             }
-        
+
             disp += numProcs;
         }
 
@@ -156,9 +152,12 @@ int main(int argc, char* argv[])
             CHUNKxCHUNK_RE,
             0,
             MPI_COMM_WORLD
-            );
+        );
 #ifdef DEBUG
-        if(rankID == 0) printf("Proc: MA\tJob: Gather [# %d]\n", CUR_CHUNK / numProcs);
+
+        if(rankID == 0)
+            printf("Proc: MA\tJob: Gather [# %d]\n", CUR_CHUNK / numProcs);
+
 #endif
     }
 
@@ -167,28 +166,27 @@ int main(int argc, char* argv[])
     elapsed_time = stop - start;
 
     /** Master process plot the image */
-    if(rankID == 0)
-    {
+    if(rankID == 0) {
         printf("Algorithm completed for,\n\t%d * %d pixels\n\t%d maximum iterations\n\t\tin %f seconds.\n",
-                FULL_WIDTH, FULL_WIDTH,
-                MAX_ITER,
-                elapsed_time);
+               FULL_WIDTH, FULL_WIDTH,
+               MAX_ITER,
+               elapsed_time);
 
-		plot(full_arr, img);
-        
+        plot(full_arr, img);
+
         fclose(img);
         free(full_arr);
     }
 
-	free(send_arr);
+    free(send_arr);
 
     /** MPI clean-up */
     MPI_Type_free(&CHUNKxCHUNK_RE);
-	MPI_Finalize();
-	fflush(stdout);
+    MPI_Finalize();
+    fflush(stdout);
 
-	/** Successful return */
-	return 0;
+    /** Successful return */
+    return 0;
 }
 
 /**
@@ -196,20 +194,20 @@ Iterating function
 */
 long iterator(Complex c, double im, double re)
 {
-	Complex z;
-	long itCount = 0;
+    Complex z;
+    long itCount = 0;
 
-	z.re = re;
-	z.im = im;
+    z.re = re;
+    z.im = im;
 
-	for(; itCount < MAX_ITER; itCount++)
-	{
-		z = cmplx_add(cmplx_squared(z), c);
-		if(cmplx_magnitude(z) > 4)
-			break;
-	}
+    for(; itCount < MAX_ITER; itCount++) {
+        z = cmplx_add(cmplx_squared(z), c);
 
-	return itCount + 1;
+        if(cmplx_magnitude(z) > 4)
+            break;
+    }
+
+    return itCount + 1;
 }
 
 /**
@@ -217,29 +215,26 @@ Function which calculates the pixel values from the square array
 */
 void plot(int* full_arr, FILE* img)
 {
-	int i, j;
-	unsigned char line[3 * FULL_WIDTH];
+    int i, j;
+    unsigned char line[3 * FULL_WIDTH];
 
-	for(i = 0; i < FULL_WIDTH; i++)
-	{
-		for(j = 0; j < FULL_WIDTH; j++)
-		{
-			if (*(full_arr + j + (i * FULL_WIDTH)) <= 63)
-			{
-				line[3 * j] = 255;
-				line[3 * j + 1] = line[3 * j + 2] = 
-						255 - 4 * *(full_arr + j + (i * FULL_WIDTH));
-			}
-			else
-			{
-				line[3 * j] = 255;
-				line[3 * j + 1] = *(full_arr + j + (i * FULL_WIDTH)) - 63;
-				line[3 * j + 2] = 0;
-			}
-			if (*(full_arr + j + (i * FULL_WIDTH)) == 320) 
-				line[3 * j] = line[3 * j + 1] = line[3 * j + 2] = 255;
-		}
-		fwrite(line, 1, 3 * FULL_WIDTH, img);
-	}
+    for(i = 0; i < FULL_WIDTH; i++) {
+        for(j = 0; j < FULL_WIDTH; j++) {
+            if (*(full_arr + j + (i * FULL_WIDTH)) <= 63) {
+                line[3 * j] = 255;
+                line[3 * j + 1] = line[3 * j + 2] =
+                                      255 - 4 * *(full_arr + j + (i * FULL_WIDTH));
+            } else {
+                line[3 * j] = 255;
+                line[3 * j + 1] = *(full_arr + j + (i * FULL_WIDTH)) - 63;
+                line[3 * j + 2] = 0;
+            }
+
+            if (*(full_arr + j + (i * FULL_WIDTH)) == 320)
+                line[3 * j] = line[3 * j + 1] = line[3 * j + 2] = 255;
+        }
+
+        fwrite(line, 1, 3 * FULL_WIDTH, img);
+    }
 }
 
--- src/fracFun_DYNAMIC.c
+++ src/fracFun_DYNAMIC.c
@@ -1,6 +1,6 @@
 /***************************************************************************
  * Filename: fracfun_DYNAMIC.c
- * Usage: ./bin/fracfun_DYNAMIC [max_iterations] [real_part] [imaginary_part] 
+ * Usage: ./bin/fracfun_DYNAMIC [max_iterations] [real_part] [imaginary_part]
  *			[Optional: [X co-ord] [Y co-ord]]
  * Author: Benjamin J Carrington
  *
@@ -13,13 +13,13 @@
 #include "cmplx.h"
 
 /**
-Takes the information of 'image', calculates colour intensity per pixel, and 
+Takes the information of 'image', calculates colour intensity per pixel, and
 writes to 'img' handle
 */
 void plot(int** image, FILE* img, int szX, int szY);
 
 /**
-Performs the equation of z = z^2 + c; returns number of iterations before 
+Performs the equation of z = z^2 + c; returns number of iterations before
 point falls outside the circle
 */
 int iterator(Complex c, int max_iterations, double im, double re);
@@ -29,112 +29,96 @@ Main function
 */
 int main(int argc, char* argv[])
 {
-	/** Double pointer to be used as 2d array for the image */
+    /** Variable declarations */
     int **image;
-    /** X and Y co-ordinates with default values */
-	int szX = 500, szY = 500;
-    /** Maximum number of iterations for algorithm */
+    int szX = 500, szY = 500;
     int max_iterations;
-    /** Counters */
-	int i, j;
-    /** FILE handle */
-	FILE *img;
-    /** Constant 'c' of type Complex (struct) */
-	Complex c;
-    /** Pointers to end characters of string conversions */
-	char *itEnd_p, *imEnd_p, *reEnd_p, *szXEnd_p, *szYEnd_p;
-    /** Clock variables for timing purposes */
-	clock_t start, finish;
+    int i, j;
+    FILE *img;
+    Complex c;
+    char *itEnd_p, *imEnd_p, *reEnd_p, *szXEnd_p, *szYEnd_p;
+    clock_t start, finish;
     float elapsed_time;
 
-	/** Assure correct # of arguments */
-	if(argc != 4 && argc != 6)
-	{
-		printf("Incorrect number of arguments\nUsage:\n\tfractal [max_iterations] [real_part] [imaginary_part]\n");
-		return 1;
-	}
-	
-	/** User-input section */
-	max_iterations = strtol(argv[1], &itEnd_p, 10);
-	c.re = strtod(argv[2], &reEnd_p);
-	c.im = strtod(argv[3], &imEnd_p);
+    /** Assure correct # of arguments */
+    if(argc != 4 && argc != 6) {
+        printf("Incorrect number of arguments\nUsage:\n\tfractal [max_iterations] [real_part] [imaginary_part]\n");
+        return 1;
+    }
+
+    /** User-input section */
+    max_iterations = strtol(argv[1], &itEnd_p, 10);
+    c.re = strtod(argv[2], &reEnd_p);
+    c.im = strtod(argv[3], &imEnd_p);
 
     /** If X and Y has been user-inputted, convert them to int and store */
-	if(argc == 6) 
-	{
-		szX = strtol(argv[4], &szXEnd_p, 10);
-		szY = strtol(argv[5], &szYEnd_p, 10);
-	}
+    if(argc == 6) {
+        szX = strtol(argv[4], &szXEnd_p, 10);
+        szY = strtol(argv[5], &szYEnd_p, 10);
+    }
 
     /** If unexpected characters are caught by conversions, end the program */
-	if(*itEnd_p || *imEnd_p || *reEnd_p) 
-	{
-		printf("Non-numeric characters in input\nProgram ending\n");
-		return 1;
-	}
-	
-    /** Allocate memory for 'image' */
-	image = (int **)malloc(szX * sizeof(int *));
-	for(i = 0; i < szX; i++)
-		image[i] = (int *)malloc(szY * sizeof(int));
+    if(*itEnd_p || *imEnd_p || *reEnd_p) {
+        printf("Non-numeric characters in input\nProgram ending\n");
+        return 1;
+    }
 
-	/** Open 'img' handle as 'overwrite if exists' */
-	img=fopen("image_out.ppm","w");
-    if(img == NULL)
-    {
+    /** Allocate memory for 'image' */
+    image = (int **)malloc(szX * sizeof(int *));
+
+    for(i = 0; i < szX; i++)
+        image[i] = (int *)malloc(szY * sizeof(int));
+
+    /** Open 'img' handle as 'overwrite if exists' */
+    img = fopen("image_out.ppm", "w");
+
+    if(img == NULL) {
         printf("Could not open output file. Exiting\n");
         return 1;
     }
 
     /** Print file signature to handle */
-	fprintf(img,"P6\n%d %d 255\n", szX, szY);
-    if(img == NULL)
-    {
+    fprintf(img, "P6\n%d %d 255\n", szX, szY);
+
+    if(img == NULL) {
         printf("Could not open image file\n");
         return 1;
     }
 
-	/** Begin the clock */
-	start = clock();
+    /** Begin the clock */
+    start = clock();
 
-    /** For all values of szX */
-	for(i = 0; i < szY; i++)
-	{
-        /** For all values of szY */
-		for(j = 0; j < szX; j++)
-		{
+    for(i = 0; i < szY; i++) {
+        for(j = 0; j < szX; j++) {
             /** Call iterator function with each pixel (mapped between -1 and 1) */
             image[j][i] = iterator(c, max_iterations, \
-					-(((i - (szY / 2)) / (double) szY) * 2), 
-					((j - (szX / 2)) / (double) szX) * 2);			
-		}	
-	}
+                                   -(((i - (szY / 2)) / (double) szY) * 2),
+                                   ((j - (szX / 2)) / (double) szX) * 2);
+        }
+    }
 
-	/** End the clock */
-	finish = clock() - start;
+    /** End the clock */
+    finish = clock() - start;
+    elapsed_time = ((float) finish) / ((float) CLOCKS_PER_SEC);
 
-    /** Convert clocks/second to seconds (as float) */
-	elapsed_time = ((float) finish) / ((float) CLOCKS_PER_SEC);
-	
     /** Print information regarding algorithm and run time */
-	printf("Algorithm completed for,\n\t%d * %d pixels\n\t%d maximum iterations\n\t\tin %f seconds.\n", \
-			szX, szY, \
-			max_iterations, \
-			elapsed_time);
-	
-	/** Plot the image */
-	plot(image, img, szX, szY);
+    printf("Algorithm completed for,\n\t%d * %d pixels\n\t%d maximum iterations\n\t\tin %f seconds.\n", \
+           szX, szY, \
+           max_iterations, \
+           elapsed_time);
 
-	/** Close handle to 'img' */
-	fclose(img);
+    /** Plot the image */
+    plot(image, img, szX, szY);
 
-    /** Free memory for 'image' */
-	for(i = 0; i < szX; i++)
-		free(image[i]);
-	free(image);
+    fclose(img);
+
+    for(i = 0; i < szX; i++)
+        free(image[i]);
+
+    free(image);
 
     /** Successful return */
-	return 0;
+    return 0;
 }
 
 /**
@@ -143,27 +127,23 @@ Iterating function
 int iterator(Complex c, int max_iterations, double re, double im)
 {
     /** Variable 'z' of type Complex structure */
-	Complex z;
-    /** Counter of iterations */
-	int itCount;
+    Complex z;
+    int itCount;
 
     /** Setting 'z' equal to the incoming grid co-ordinates */
-	z.re = re;
-	z.im = im;
+    z.re = re;
+    z.im = im;
 
     /** Main loop of the algorithm which is ran 'max_iterations' number of times */
-	for(itCount = 0; itCount < max_iterations; itCount++)
-	{
-        /** Perform z = z^2 + c */
-		z = cmplx_add(cmplx_squared(z), c);
+    for(itCount = 0; itCount < max_iterations; itCount++) {
+        z = cmplx_add(cmplx_squared(z), c);
 
-        /** If components of z squared are > 4, break */
-		if(cmplx_magnitude(z) > 4)
-			break;
-	}
+        if(cmplx_magnitude(z) > 4)
+            break;
+    }
 
     /** Return the number of iterations */
-	return itCount;
+    return itCount;
 }
 
 /**
@@ -172,32 +152,27 @@ Plotting function
 void plot(int** image, FILE* img, int szX, int szY)
 {
     /** Counters */
-	int i, j;
+    int i, j;
     /** Array of byte values of the width of X dimension multiplied by three to accomodate
     one byte each for R, G, and B */
-	unsigned char line[3 * szX];
+    unsigned char line[3 * szX];
 
-    /** For the length of Y */
-	for(i = 0; i < szY; i++)
-	{
-        /** For the length of X */
-		for(j = 0; j < szX; j++)
-		{
-			if (image[j][i] <= 63)
-			{
-				line[3 * j] = 255;
-				line[3 * j + 1] = line[ 3 * j + 2 ] = 255 - 4 * image[j][i];
-			}
-			else
-			{
-				line[3 * j] = 255;
-				line[3 * j + 1] = image[j][i] - 63;
-				line[3 * j + 2] = 0;
-			}
-			if (image[j][i] == 320)	
+    for(i = 0; i < szY; i++) {
+        for(j = 0; j < szX; j++) {
+            if (image[j][i] <= 63) {
+                line[3 * j] = 255;
+                line[3 * j + 1] = line[ 3 * j + 2 ] = 255 - 4 * image[j][i];
+            } else {
+                line[3 * j] = 255;
+                line[3 * j + 1] = image[j][i] - 63;
+                line[3 * j + 2] = 0;
+            }
+
+            if (image[j][i] == 320)
                 line[3 * j] = line[3 * j + 1] = line[3 * j + 2] = 255;
-		}
+        }
+
         /** Write 'line' array to 'img' handle */
-		fwrite(line, 1, 3 * szX, img);
-	}
+        fwrite(line, 1, 3 * szX, img);
+    }
 }
